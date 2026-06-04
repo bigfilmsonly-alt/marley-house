bf46286442cc6d078caf7c7234620f343260b004
bigfilmsonly-alt <bigfilmsonly@gmail.com>
2026-06-04 18:11:11 -0400

    Bigger center logo (130px)

--- app/(shell)/layout.tsx
+++ app/(shell)/layout.tsx
@@ -32,7 +32,7 @@ export default function ShellLayout({
           <div className="flex-shrink-0 pt-3 pb-1 bg-black">
             <div className="flex items-center justify-center gap-3 mb-2 px-4">
               <img src="/brand/lion-head-gold.png" alt="Lion Order" width="100" height="100" className="brightness-125" />
-              <img src="/brand/rastafari-lion-university.png" alt="Rastafari Lion Order University" width="100" height="100" className="brightness-125" />
+              <img src="/brand/rastafari-lion-university.png" alt="Rastafari Lion Order University" width="130" height="130" className="brightness-125" />
               <img src="/brand/lion-crest-clean.png" alt="Lion Order Crest" width="100" height="100" className="brightness-125" />
             </div>
             <p style={{ color: '#E8C23A', fontSize: '9px', letterSpacing: '0.4em', textTransform: 'uppercase', textAlign: 'center', fontWeight: 500, marginBottom: '2px' }}>
